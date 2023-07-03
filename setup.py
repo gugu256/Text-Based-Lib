4999bfad7a2f6062da32f0c632d71e4cb59d8da4
PancakeDev <gustave.dufresne.walter@gmail.com>
2023-07-04 01:44:37 +0200

    Finallyisch

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '1.2'
+DESCRIPTION = 'A library for creating text-based games in the terminal'
+
+# Setting up
+setup(
+    name="text-based-lib",
+    version=VERSION,
+    author="gugu256 (Gustave Dufresne-Walter)",
+    author_email="gugu256@mail.com",
+    url="https://tblib-docs.devplodocus.repl.co",
+    license="MIT License",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['colorama'],
+    keywords=['python', 'lightweight', 'game-development', 'terminal-based', 'terminal-game', 'text-based-game'],
+    classifiers=[
+        "Development Status :: Releasing",
+        "Intended Audience :: Developers/Game developers/Beginners",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
---
+++ text-based-lib/__init__.py
@@ -0,0 +1,1 @@
+from tblib import *
